0ba3932b3818bc32d15808765858b85a03e15aef
Joel Alexander <joel.lewis.alexander@gmail.com>
2024-07-05 11:26:22 -0400

    .

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV == 'development'
     ? `http://localhost:3000`
-    : 'https://puny-kilobyte-purring.on-fleek.app';
+    : 'https://enough-manchester-limited.on-fleek.app';
 export const NEXT_PUBLIC_CDP_API_KEY = process.env.NEXT_PUBLIC_CDP_API_KEY;
